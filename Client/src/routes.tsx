6aabd878ff99e2f38adf276fe64e7128bc55eee3
bou1der <krowkin@bk.ru>
2024-04-23 11:54:15 +0300

    init client

--- Client/src/routes.tsx
+++
@@ -1,9 +0,0 @@
-
-const router = () =>{
-    return(
-        <>
-
-        </>
-    )
-}
-export default router
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import config from './config.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    port:config.client.port
-  }
 })
